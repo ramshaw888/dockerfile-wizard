252022b27f2ecc681aeb7c15396ba9cda6a0a1a1
Aaron Ramshaw <aaron.ramshaw@domain.com.au>
2018-04-11 11:12:37 +1000

    Remove unneeded stuff from dockerfile

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -5,7 +5,7 @@ PYTHON_2_VERSION="2.7.14"
 
 echo "FROM buildpack-deps:$(awk -F'_' '{print tolower($2)}' <<< $LINUX_VERSION)"
 
-RUN apt-get update
+echo "RUN apt-get update"
 
 echo "RUN wget https://www.python.org/ftp/python/$PYTHON_3_VERSION/Python-$PYTHON_3_VERSION.tgz && \
 tar xzf Python-$PYTHON_3_VERSION.tgz && \
@@ -21,21 +21,7 @@ cd Python-$PYTHON_2_VERSION && \
 ./configure && \
 make altinstall"
 
-# install lsb-release, etc., for testing linux distro
-echo "RUN apt-get update && apt-get -y install lsb-release unzip"
-
 echo "RUN apt-get install -y python-dev"
 echo "RUN python3 -m ensurepip --default-pip"
 echo "RUN pip3 install --upgrade pip"
 echo "RUN pip3 install 'tox>=3.0.0rc4' tox-battery"
-
-# install bats for testing
-echo "RUN git clone https://github.com/sstephenson/bats.git \
-  && cd bats \
-  && ./install.sh /usr/local \
-  && cd .. \
-  && rm -rf bats"
-
-# install dependencies for tap-to-junit
-echo "RUN perl -MCPAN -e 'install TAP::Parser'"
-echo "RUN perl -MCPAN -e 'install XML::Generator'"
